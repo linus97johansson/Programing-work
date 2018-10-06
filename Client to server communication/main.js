17b38dba77ac2d93a01f7a38cf29ac41132aded0
linus97johansson <linus97johansson@gmail.com>
2018-10-06 13:51:44 +0200

    Initial commit

--- Client to server communication/main.js
+++ Client to server communication/main.js
@@ -4,12 +4,22 @@ const sqlite3 = require('sqlite3')
 
 const db = new sqlite3.Database("Database.db")
 
-const querry = "CREATE TABLE IF NOT EXISTS "
-db.run()
+const querry = `CREATE TABLE IF NOT EXISTS GestBookPost ( 
+    ID INT   NOT NULL,
+    Name VARCHAR(255)   NOT NULL,
+    Message VARCHAR(255) NOT NULL,
+    CreatedAt VARCHAR(255) NOT NULL,
+    PRIMARY KEY(ID)
+)`
+db.run(querry)
 
 app.get("/", function(req, res){
     
     res.send("Hello, World")
+})
+
+app.get("/posts/:id", function(req,res){
+
 })
 
 
